41ea33f3a72264639973127bbf2f4a9f5154c02d
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2024-01-02 09:46:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-const { ApolloServer } = require('apollo-server-express');
-const mongoose = require('mongoose');
-const resolvers = require('./resolvers');
-const typeDefs = require('./typeDefs');
+const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
+const resolvers = require('./resolvers')
+const typeDefs = require('./typeDefs')
 const express = require('express');
 
 require('dotenv').config();
 
-
+async function startServer() {
     const app = express();
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
-        introspection: true,  // Enable introspection for Apollo Studio
-        playground: true,     // Enable GraphQL Playground
     });
 
     await apolloServer.start();
     app.use(express.json());
-    apolloServer.applyMiddleware({ app, path: '/' });
+    apolloServer.applyMiddleware({ app: app, path: '/' });
 
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGO_URI , {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     })
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.error("MongoDB connection error:", error);
-    });
+        .then(() => {
+            console.log("connected to database: test (by default) -> collection name : certifind");
+        })
 
-    const port = process.env.PORT || 5000; // Use PORT environment variable or default to 5000
-    // app.listen(port, () => {
-    //     console.log(`Server running on http://localhost:${port}`);
-    // });
+    app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT }`));
+}
 
-exports.handler = apolloServer.creteHandler();
+startServer();
