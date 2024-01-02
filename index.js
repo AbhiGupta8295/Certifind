f5531e65ba81ec995457926bf83ec3fdc0cfe31d
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2024-01-02 10:18:03 +0530

    changes

---
+++ index.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
+const resolvers = require('./resolvers')
+const typeDefs = require('./typeDefs')
+const express = require('express');
+const serverless = require('serverless-http');
+
+require('dotenv').config();
+
+const app = express();
+const router = express.Router();
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+await apolloServer.start();
+app.use(express.json());
+apolloServer.applyMiddleware({ app: app, path: '/' });
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("connected to database: test (by default) -> collection name : certifind");
+    })
+
+// app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT }`));
+app.use('/.netlify/functions/', router);
+export const handler = serverless(api);
