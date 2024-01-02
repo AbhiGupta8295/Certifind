734491dffacf2110e4296ff481eb9cfa16c8069e
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2024-01-02 07:34:55 +0530

    create netlify functions

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function startServer() {
     app.use(express.json());
     apolloServer.applyMiddleware({ app: app, path: '/' });
 
-    mongoose.connect(process.env.MONGO_URI , {
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
