ee541e59792c734a6f62870222f9c17fb6205d0b
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2024-06-17 15:38:29 +0530

    Create api.js

---
+++ functions/api.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
+const resolvers = require('../resolvers')
+const typeDefs = require('../typeDefs')
+const express = require('express');
+
+require('dotenv').config();
+
+async function startServer() {
+    const app = express();
+    app.use(cors({origin: '*'}));
+    app.use(bodyParser.json());
+    app.use(express.json());
+    const router = express.Router();
+
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    await apolloServer.start();
+    app.use(express.json());
+    apolloServer.applyMiddleware({ app: app, path: '/.netlify/functions/api' });
+
+    mongoose.connect(process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.yb17mkv.mongodb.net/", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+            console.log("connected to database: test (by default) -> collection name : certifind");
+        })
+
+    // app.listen(process.env.PORT || 5001, () => console.log(`Server running on http://localhost:${process.env.PORT }`));
+    app.use('/.netlify/functions/api',router);
+
+}
+
+startServer();
